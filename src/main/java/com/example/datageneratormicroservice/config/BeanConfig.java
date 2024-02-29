a431769a311fabdfe0a84053d47cccbfec521ea4
Yana_Aziamblouskaya <yanaozembl@gmail.com>
2024-02-29 21:26:29 +0300

    initial commit

---
+++ src/main/java/com/example/datageneratormicroservice/config/BeanConfig.java
@@ -0,0 +1,22 @@
+package com.example.datageneratormicroservice.config;
+
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    @SneakyThrows
+    public XML produverXML() {
+        return new XMLDocument(
+                getClass().getResourceAsStream("/kafka/consumer.xml").
+                        readAllBytes()
+        );
+    }
+}
